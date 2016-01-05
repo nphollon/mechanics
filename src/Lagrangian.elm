d591c193f721f3ef0b971f88b732fa421c320937
Nick Hollon <falling.masonry@gmail.com>
2016-01-05 10:55:11 -0800

    Create state object

--- src/Lagrangian.elm
+++ src/Lagrangian.elm
@@ -1,3 +1,57 @@
-module Lagrangian where
+module Lagrangian (state1, state2, state3, state
+                  , dimension, time, coordinate, velocity
+                  ) where
 
-dummy = 0
+import Array exposing (Array)
+
+
+state1 : (Float, Float) -> State
+state1 coord =
+  state 0 (Array.repeat 1 coord)
+
+
+state2 : (Float, Float) -> (Float, Float) -> State
+state2 x y =
+  state 0 (Array.fromList [ x, y ])
+
+
+state3 : (Float, Float) -> (Float, Float) -> (Float, Float) -> State
+state3 x y z =
+  state 0 (Array.fromList [ x, y, z ])
+
+        
+state : Float -> Array (Float, Float) -> State
+state time coords =
+  { time = time
+  , coords = coords
+  }
+
+
+dimension : State -> Int
+dimension state =
+  Array.length state.coords
+
+
+time : State -> Float
+time =
+  .time
+
+
+coordinate : Int -> State -> Float
+coordinate i state =
+  Array.get i state.coords
+    |> Maybe.map fst
+    |> Maybe.withDefault 0
+
+
+velocity : Int -> State -> Float
+velocity i state =
+  Array.get i state.coords
+    |> Maybe.map snd
+    |> Maybe.withDefault 0
+
+            
+type alias State =
+  { time : Float
+  , coords : Array (Float, Float)
+  }
--- src/Test/Lagrangian.elm
+++ src/Test/Lagrangian.elm
@@ -1,31 +1,95 @@
 module Test.Lagrangian where
 
+import Array
+
 import ElmTest exposing (..)
 
 import Lagrangian as Mech
 
 all : Test
 all =
-  suite "Lagrangian" [
+  suite "Dynamics"
+          [ suite "Building states" stateTests
+          , suite "Evolving states" accelerationTests
           ]
 
 
-spec : Test
-spec =
-  let
-    lagrangian = sum [ (square (velocity 0))
-                     , (square (velocity 1))
-                     , (times (num -1) (coordinate 1))
-                     ]
-                 
-    acceleration = Mech.acceleration lagrangian
-    initState = Mech.state1D 0 (0, 2)
-    laterState = Mech.evolve 0.1 acceleration initState
-  in
-    assertEqual laterState initState
+stateTests : List Test
+stateTests =
+  [ test "state1 is sugar for 1-dimensional state"
+    <| assertEqual
+         (Mech.state1 (1, 2))
+         (Mech.state 0 (Array.fromList [(1, 2)]))
+
+  , test "states are unequal if their coordinates differ"
+    <| assertNotEqual
+         (Mech.state1 (1, 2))
+         (Mech.state 0 (Array.fromList [(2, 2)]))
+
+  , test "states are unequal if their times differ"
+    <| assertNotEqual
+         (Mech.state1 (1, 2))
+         (Mech.state 0.5 (Array.fromList [(1, 2)]))
+
+  , test "state2 is sugar for 2-dimensional state"
+    <| assertEqual
+         (Mech.state2 (4, 2) (3, 1))
+         (Mech.state 0 (Array.fromList [(4, 2), (3, 1)]))
+         
+  , test "state3 is sugar for 3-dimensional state"
+    <| assertEqual
+         (Mech.state3 (8, 9) (4, 2) (3, 1))
+         (Mech.state 0 (Array.fromList [(8, 9), (4, 2), (3, 1)]))
+
+  , test "dimension returns number of coordinates"
+    <| assertEqual
+         2
+         (Mech.dimension (Mech.state2 (0, 0) (0, 0)))
+
+  , test "time returns time"
+    <| assertEqual
+         1.5
+         (Mech.time (Mech.state 1.5 Array.empty))
+
+  , test "coordinate returns coordinate with given index"
+    <| assertEqual
+         5
+         (Mech.coordinate 1 (Mech.state2 (0, 0) (5, 0)))
+
+  , test "coordinate returns 0 if index is out of range"
+    <| assertEqual
+         0
+         (Mech.coordinate 2 (Mech.state2 (0, 0) (5, 0)))
+
+  , test "velocity returns velocity with given index"
+    <| assertEqual
+         7
+         (Mech.velocity 1 (Mech.state2 (0, 0) (0, 7)))
+  ]
 
 
+accelerationTests : List Test
+accelerationTests =
+  []
+  
+{-
+state1 : (Float, Float) -> State
+state2 : (Float, Float) -> (Float, Float) -> State
+state3 : (Float, Float) -> (Float, Float) -> (Float, Float) -> State
+state : Float -> Array (Float, Float) -> State
 
+velocity : Int -> State -> Float
+coordinate : Int -> State -> Float
+time : State -> Float
+arity : State -> Int
+
+lagrangianToAcceleration : Expression -> Acceleration
+
+acceleration : (State -> State) -> Acceleration
+
+evolve : Float -> Acceleration -> State -> State
+-}
+{-
 num : Float -> Expression
 plus : Expression -> Expression -> Expression
 minus : Expression -> Expression -> Expression
@@ -42,13 +106,7 @@ product : List Expression -> Expression
 
 velocity : Int -> Expression
 coordinate : Int -> Expression
-time : Int -> Expression
+time : Expression
 
-state1 : (Float, Float) -> State
-state2 : (Float, Float) -> (Float, Float) -> State
-state3 : (Float, Float) -> (Float, Float) -> (Float, Float) -> State
-state : Float -> Array (Float, Float) -> State
-
-findAcceleration : Expression -> Acceleration
-
-evolve : Float -> Acceleration -> State -> State
+eval : Expression -> State -> State
+-}
