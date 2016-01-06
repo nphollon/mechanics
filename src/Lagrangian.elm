1de2b24c997553a08a8ee5f2213cee09f962145e
Nick Hollon <falling.masonry@gmail.com>
2016-01-05 21:39:18 -0800

    Documentation outline

--- src/Lagrangian.elm
+++ src/Lagrangian.elm
@@ -3,34 +3,62 @@ module Lagrangian (State, Acceleration
                   , aboutEqual, dimension, time, coordinate, velocity
                   , evolve, acceleration) where
 
+{-|
+# Building states
+@docs State, state1, state2, state3, state
+
+# Inspecting states
+@docs aboutEqual, dimension, time, coordinate, velocity
+
+# Changing states
+@docs Acceleration, acceleration, evolve
+-}
 import Array exposing (Array)
 
 
+-- Building states
+
+{-|-}
+type State =
+  Data 
+  { time : Float
+  , coordinates : List Float
+  , velocities : List Float
+  }
+
+           
+{-|-}
 state1 : (Float, Float) -> State
 state1 x =
   state 0 [ x ]
 
 
+{-|-}
 state2 : (Float, Float) -> (Float, Float) -> State
 state2 x y =
   state 0 [ x, y ]
 
 
+{-|-}
 state3 : (Float, Float) -> (Float, Float) -> (Float, Float) -> State
 state3 x y z =
   state 0 [ x, y, z ]
 
         
+{-|-}
 state : Float -> List (Float, Float) -> State
 state time coords =
   { time = time
   , coordinates = List.map fst coords
   , velocities = List.map snd coords
-  }
+  } |> Data
 
 
+-- Inspecting states
+
+{-|-}
 aboutEqual : Float -> State -> State -> Bool
-aboutEqual tolerance a b =
+aboutEqual tolerance (Data a) (Data b) =
   let
     eq x y =
       (x - y)^2 < tolerance^2
@@ -44,35 +72,48 @@ aboutEqual tolerance a b =
     (eqAll a.velocities b.velocities)
     
   
+{-|-}
 dimension : State -> Int
-dimension state =
+dimension (Data state) =
   List.length state.coordinates
 
 
+{-|-}
 time : State -> Float
-time =
-  .time
+time (Data state) =
+  state.time
 
 
+{-|-}
 coordinate : Int -> State -> Float
-coordinate i state =
+coordinate i (Data state) =
   Array.fromList state.coordinates
     |> Array.get i
     |> Maybe.withDefault 0
 
 
+{-|-}
 velocity : Int -> State -> Float
-velocity i state =
+velocity i (Data state) =
   Array.fromList state.velocities
     |> Array.get i 
     |> Maybe.withDefault 0
 
 
+-- Evolving states
+
+{-|-}
+type Acceleration =
+  Force (State -> List Float)
+
+
+{-|-}
 acceleration : (State -> List Float) -> Acceleration
 acceleration a =
-  a
+  Force a
 
   
+{-|-}
 evolve : Acceleration -> Float -> State -> State
 evolve accel dt state =
   let
@@ -89,7 +130,7 @@ evolve accel dt state =
 
 
 nudge : Float -> State -> State -> State
-nudge dt derivative state =
+nudge dt (Data derivative) (Data state) =
   let
     add dxdt x =
       x + dt * dxdt
@@ -100,23 +141,12 @@ nudge dt derivative state =
     { state
       | coordinates = combine .coordinates
       , velocities = combine .velocities
-    }
+    } |> Data
       
 
 stateDerivative : Acceleration -> State -> State
-stateDerivative accel state =
+stateDerivative (Force accel) (Data state) =
   { state
     | coordinates = state.velocities
-    , velocities = accel state
-  }
-  
-
-type alias State =
-  { time : Float
-  , coordinates : List Float
-  , velocities : List Float
-  }
-
-                 
-type alias Acceleration =
-  State -> List Float
+    , velocities = accel (Data state)
+  } |> Data
