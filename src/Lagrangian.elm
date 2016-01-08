b35b0f8e9ec335dbbf7530103485f952ba0afd19
Nick Hollon <falling.masonry@gmail.com>
2016-01-07 21:28:38 -0800

    Partial differentiation

--- src/Lagrangian.elm
+++ src/Lagrangian.elm
@@ -1,4 +1,4 @@
-module Lagrangian (num, time, coordinate, velocity, plus, minus, times, over, sum, product, square, expt, sine, cosine, ln, eval, Expression) where
+module Lagrangian (num, time, coordinate, velocity, plus, minus, times, over, sum, product, square, expt, sine, cosine, ln, eval, partial, Expression) where
 
 import Mechanics as Mech exposing (State)
 
@@ -38,22 +38,91 @@ velocity i =
 
 plus : Expression -> Expression -> Expression
 plus a b =
-    Sum [ a, b ]
+    sum [ a, b ]
 
 
 minus : Expression -> Expression -> Expression
 minus a b =
-    a `plus` ((num -1) `times` b)
+    sum [ a, (num -1) `times` b ]
+
+
+sum : List Expression -> Expression
+sum terms =
+    let
+        addTerm term ( const, symbols ) =
+            case term of
+                Const n ->
+                    ( n + const, symbols )
+
+                Sum ((Const n) :: subsymbols) ->
+                    ( n + const, subsymbols ++ symbols )
+
+                Sum subsymbols ->
+                    ( const, subsymbols ++ symbols )
+
+                otherwise ->
+                    ( const, term :: symbols )
+
+        reduced = List.foldr addTerm ( 0, [] ) terms
+    in
+        case reduced of
+            ( c, [] ) ->
+                num c
+
+            ( 0, x :: [] ) ->
+                x
+
+            ( 0, xs ) ->
+                Sum xs
+
+            ( c, xs ) ->
+                Sum ((num c) :: xs)
 
 
 times : Expression -> Expression -> Expression
 times a b =
-    Prod [ a, b ]
+    product [ a, b ]
 
 
 over : Expression -> Expression -> Expression
 over a b =
-    a `times` (b `expt` (num -1))
+    product [ a, b `expt` (num -1) ]
+
+
+product : List Expression -> Expression
+product factors =
+    let
+        addFactor factor ( const, symbols ) =
+            case factor of
+                Const n ->
+                    ( n * const, symbols )
+
+                Prod ((Const n) :: subsymbols) ->
+                    ( n * const, subsymbols ++ symbols )
+
+                Prod subsymbols ->
+                    ( const, subsymbols ++ symbols )
+
+                otherwise ->
+                    ( const, factor :: symbols )
+
+        reduced = List.foldr addFactor ( 1, [] ) factors
+    in
+        case reduced of
+            ( 0, _ ) ->
+                num 0
+
+            ( c, [] ) ->
+                num c
+
+            ( 1, x :: [] ) ->
+                x
+
+            ( 1, xs ) ->
+                Prod xs
+
+            ( c, xs ) ->
+                Prod ((num c) :: xs)
 
 
 square : Expression -> Expression
@@ -63,7 +132,15 @@ square base =
 
 expt : Expression -> Expression -> Expression
 expt base power =
-    Pow base power
+    case power of
+        Const 0 ->
+            num 1
+
+        Const 1 ->
+            base
+
+        otherwise ->
+            Pow base power
 
 
 sine : Expression -> Expression
@@ -79,16 +156,6 @@ cosine x =
 ln : Expression -> Expression
 ln x =
     Log x
-
-
-sum : List Expression -> Expression
-sum terms =
-    Sum terms
-
-
-product : List Expression -> Expression
-product factors =
-    Prod factors
 
 
 eval : Expression -> State -> Float
@@ -123,3 +190,58 @@ eval expr state =
 
         Cos x ->
             cos (eval x state)
+
+
+partial : Expression -> Expression -> Expression
+partial variable function =
+    let
+        recurse = partial variable
+    in
+        if function == variable then
+            num 1
+        else
+            case function of
+                Const _ ->
+                    num 0
+
+                Time ->
+                    num 0
+
+                Coord _ ->
+                    num 0
+
+                Vel _ ->
+                    num 0
+
+                Prod factors ->
+                    let
+                        productTerm u =
+                            product ((recurse u) :: (List.filter ((/=) u) factors))
+                    in
+                        List.map productTerm factors |> sum
+
+                Sum terms ->
+                    List.map recurse terms |> sum
+
+                Pow base power ->
+                    sum
+                        [ product
+                            [ power
+                            , expt base (power `minus` (num 1))
+                            , partial variable base
+                            ]
+                        , product
+                            [ ln base
+                            , expt base power
+                            , partial variable power
+                            ]
+                        ]
+
+                Sin u ->
+                    (cosine u) `times` (recurse u)
+
+                Cos u ->
+                    product [ num -1, sine u, recurse u ]
+
+                Log u ->
+                    (expt u (num -1)) `times` (recurse u)
--- test/LagrangianTest.elm
+++ test/LagrangianTest.elm
@@ -10,9 +10,56 @@ all : Test
 all =
     suite
         "Lagrangian mechanics"
-        [ suite "Evaluating expressions" expressionTests
+        [ suite "Simplifying expressions" simplifyTests
+        , suite "Evaluating expressions" expressionTests
+        , suite "Partial differentiation" partialTests
         , suite "Solving Lagrange equations" lagrangeTests
         ]
+
+
+simplifyTests : List Test
+simplifyTests =
+    [ test "multiplying 0 by x"
+        <| assertEqual
+            (num 0)
+            (product [ num 0, time ])
+    , test "multiplying x by 0"
+        <| assertEqual
+            (num 0)
+            (product [ time, num 0 ])
+    , test "multiplying 1 by x"
+        <| assertEqual
+            time
+            (product [ num 1, time ])
+    , test "multiplying x by 1"
+        <| assertEqual
+            time
+            (product [ time, num 1 ])
+    , test "adding 0 to x"
+        <| assertEqual
+            time
+            (sum [ num 0, time ])
+    , test "summing to 0"
+        <| assertEqual
+            (num 0)
+            (sum [ num 1, num -1 ])
+    , test "power of one"
+        <| assertEqual
+            time
+            (expt time (num 1))
+    , test "power of zero"
+        <| assertEqual
+            (num 1)
+            (expt time (num 0))
+    , test "nested multiplication"
+        <| assertEqual
+            (product [ num 2, time, velocity 0, coordinate 1 ])
+            (num 2 `times` time `times` (velocity 0 `times` coordinate 1))
+    , test "nested addition"
+        <| assertEqual
+            (sum [ num 2, time, velocity 0, coordinate 1 ])
+            (num 2 `plus` time `plus` (velocity 0 `plus` coordinate 1))
+    ]
 
 
 expressionTests : List Test
@@ -91,16 +138,70 @@ expressionTests =
         ]
 
 
-
-{-
-sum : List Expression -> Expression
-product : List Expression -> Expression
--}
-
-
 assertEval : Float -> Expression -> State -> Assertion
 assertEval expected expr state =
     assertEqual expected (eval expr state)
+
+
+partialTests : List Test
+partialTests =
+    [ test "derivative of constant is zero"
+        <| assertEqual
+            (num 0)
+            (partial time (num 1))
+    , test "dx/dx = 1"
+        <| assertEqual
+            (num 1)
+            (partial time time)
+    , test "d(sin x)/dx = cos x"
+        <| assertEqual
+            (cosine time)
+            (partial time (sine time))
+    , test "d(ln x)/dx = 1/x"
+        <| assertEqual
+            (expt time (num -1))
+            (partial time (ln time))
+    , test "chain rule: d(sin (ln x))/dx"
+        <| assertEqual
+            ((cosine (ln time)) `times` (expt time (num -1)))
+            (partial time (sine (ln time)))
+    , test "chain rule: d(ln (sin x))/dx"
+        <| assertEqual
+            ((expt (sine time) (num -1)) `times` (cosine time))
+            (partial time (ln (sine time)))
+    , test "d(cos x)/dx = -sin x"
+        <| assertEqual
+            (product [ num -1, sine (ln time), expt time (num -1) ])
+            (partial time (cosine (ln time)))
+    , test "d(x^3)/dx = 3 * x^2"
+        <| assertEqual
+            (product [ num 3, expt time (num 2) ])
+            (partial time (expt time (num 3)))
+    , test "d(2^x)/dx = (ln 2) * 2^x"
+        <| assertEqual
+            ((ln (num 2)) `times` (expt (num 2) time))
+            (partial time (expt (num 2) time))
+    , test "d(f + g)/dx = df/dx + dg/dx"
+        <| assertEqual
+            (((num 2) `times` time) `plus` (num 1))
+            (partial time ((square time) `plus` time))
+    , test "d(f * g)/dx = g * df/dx + f * dg/dx"
+        <| assertEqual
+            (sum
+                [ product [ ln (num 2), expt (num 2) time, expt time (num 2) ]
+                , product [ num 2, time, expt (num 2) time ]
+                ]
+            )
+            (partial time ((expt (num 2) time) `times` (expt time (num 2))))
+    , test "partial wrt time ignores other variables"
+        <| assertEqual
+            (coordinate 0 `times` velocity 0)
+            (partial time (product [ time, coordinate 0, velocity 0 ]))
+    , test "partial wrt coordinate not equal to partial wrt time"
+        <| assertEqual
+            time
+            (partial (coordinate 0) (time `times` (coordinate 0)))
+    ]
 
 
 
